e6e45d295c41f2222b2dd33fa223fe7b55fea0f8
orash000 <orash.axmedov000@example.com>
2023-11-07 10:51:59 +0500

    Gyro Strength Changed

--- js/parallax.js
+++ js/parallax.js
@@ -16,37 +16,18 @@ document.addEventListener('mousemove', (e) => {
   bg.style.transform = `translate(${moveX}px, ${moveY}px)`
 })
 
-// Эффект параллакса с помощью движения пальцев
-// document.addEventListener('touchmove', (e) => {
-//   const touch = e.touches[0]
-//   const parallaxStrength = 30
-
-//   if (touch) {
-//     const touchX = touch.clientX
-//     const touchY = touch.clientY
-
-//     const centerX = window.innerWidth / 2
-//     const centerY = window.innerHeight / 2
-
-//     const moveX = (touchX - centerX) / parallaxStrength
-//     const moveY = (touchY - centerY) / parallaxStrength
-
-//     bg.style.transform = `translate(${moveX}px, ${moveY}px)`
-//   }
-// })
-
 // Эффект параллакса с помощью гироскопа
 if (window.DeviceOrientationEvent) {
   window.addEventListener('deviceorientation', handleOrientation)
 } else {
-  console.log('Гироскоп не поддерживается на этом устройстве.')
+  console.log('Гироскоп не поддерживается')
   alert("Гироскоп не поддерживается")
 }
 
 function handleOrientation(event) {
   const beta = event.beta
   const gamma = event.gamma
-  const parallaxStrength = 10
+  const parallaxStrength = 60
 
   bg.style.transform = `translate(${gamma * parallaxStrength}px, ${beta * parallaxStrength}px)`
 }
