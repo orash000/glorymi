404e713c01301927223c1b1de59e95b142053007
orash000 <orash.axmedov000@example.com>
2023-11-07 10:26:15 +0500

    Gyroscope Added

--- js/parallax.js
+++ js/parallax.js
@@ -15,20 +15,45 @@ document.addEventListener('mousemove', (e) => {
   bg.style.transform = `translate(${moveX}px, ${moveY}px)`
 })
 
-document.addEventListener('touchmove', (e) => {
-  const touch = e.touches[0]
-  const parallaxStrength = 30
+// document.addEventListener('touchmove', (e) => {
+//   const touch = e.touches[0]
+//   const parallaxStrength = 30
 
-  if (touch) {
-    const touchX = touch.clientX
-    const touchY = touch.clientY
+//   if (touch) {
+//     const touchX = touch.clientX
+//     const touchY = touch.clientY
 
-    const centerX = window.innerWidth / 2
-    const centerY = window.innerHeight / 2
+//     const centerX = window.innerWidth / 2
+//     const centerY = window.innerHeight / 2
 
-    const moveX = (touchX - centerX) / parallaxStrength
-    const moveY = (touchY - centerY) / parallaxStrength
+//     const moveX = (touchX - centerX) / parallaxStrength
+//     const moveY = (touchY - centerY) / parallaxStrength
 
-    bg.style.transform = `translate(${moveX}px, ${moveY}px)`
+//     bg.style.transform = `translate(${moveX}px, ${moveY}px)`
+//   }
+// })
+
+// Функция для обработки данных с гироскопа
+function handleGyroscope(event) {
+  const parallaxStrength = 30;
+
+  // Получить данные с гироскопа
+  const gyroscopeData = event.alpha !== null ? event : event.beta !== null ? event : event.gamma !== null ? event : null;
+
+  if (gyroscopeData) {
+    // Используйте данные с гироскопа для расчета движения
+    const moveX = gyroscopeData.gamma / parallaxStrength;
+    const moveY = gyroscopeData.beta / parallaxStrength;
+
+    // Обновите положение фона
+    bg.style.transform = `translate(${moveX}px, ${moveY}px)`;
   }
-})
+}
+
+// Проверьте, поддерживается ли устройство гироскоп
+if (window.DeviceOrientationEvent) {
+  // Добавьте слушателя события для обработки данных гироскопа
+  window.addEventListener('deviceorientation', handleGyroscope);
+} else {
+  alert("Устройство не поддерживает гироскоп.");
+}
