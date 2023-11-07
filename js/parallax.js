1eb57aa60bffbd3c79c52d48860234c310a82b00
orash000 <148531172+orash000@users.noreply.github.com>
2023-11-07 10:14:40 +0500

    Add files via upload

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,8 @@
 const bg = document.querySelector('.bg')
-const parallaxStrength = 150
 
 document.addEventListener('mousemove', (e) => {
+  const parallaxStrength = 90
+
   const mouseX = e.clientX
   const mouseY = e.clientY
 
@@ -12,4 +13,22 @@ document.addEventListener('mousemove', (e) => {
   const moveY = (mouseY - centerY) / parallaxStrength
 
   bg.style.transform = `translate(${moveX}px, ${moveY}px)`
+})
+
+document.addEventListener('touchmove', (e) => {
+  const touch = e.touches[0]
+  const parallaxStrength = 50
+
+  if (touch) {
+    const touchX = touch.clientX
+    const touchY = touch.clientY
+
+    const centerX = window.innerWidth / 2
+    const centerY = window.innerHeight / 2
+
+    const moveX = (touchX - centerX) / parallaxStrength
+    const moveY = (touchY - centerY) / parallaxStrength
+
+    bg.style.transform = `translate(${moveX}px, ${moveY}px)`
+  }
 })
