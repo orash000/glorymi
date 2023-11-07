a55f8ebe3bfbd69246899d48c6b30b579429ad02
orash000 <orash.axmedov000@example.com>
2023-11-07 13:04:52 +0500

    Fix

--- js/parallax.js
+++ js/parallax.js
@@ -29,7 +29,7 @@ function handleOrientation(event) {
   const gamma = event.gamma
   const parallaxStrength = 0.7
 
-  if (Math.abs(beta) < 30 && Math.abs(gamma) < 30) {
+  if (Math.abs(beta) < 10 && Math.abs(gamma) < 10) {
     bg.style.transform = `translate(${gamma * parallaxStrength}px, ${beta * parallaxStrength}px)`
   }
 }
